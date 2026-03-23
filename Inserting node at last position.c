275d00ae1fdad8ca9d1fdae9dea1beb0865e2ad2
tejaswirevada <tejaswirevada2006@gmail.com>
2026-03-23 15:13:16 +0530

    Add files via upload

---
+++ Inserting node at last position.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+	int data;
+	struct node *link;
+};
+struct node *head=0,*temp=0;
+void create()
+{
+	struct node *newnode=0;
+	int n,i;
+	printf("\nEnter n size:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		newnode=(struct node *)malloc(sizeof(struct node));
+		printf("Enter node %d data:",i+1);
+		scanf("%d",&newnode->data);
+		newnode->link=NULL;
+		if(head==NULL)
+		{
+			head=newnode;
+			temp=newnode;
+		}
+		else
+		{
+			temp->link=newnode;
+			temp=newnode;
+		}
+	}
+}
+void display()
+{
+	temp=head;
+	while(temp!=0)
+	{
+		printf("%d\t",temp->data);
+		temp=temp->link;
+	}
+}
+int main()
+{
+	create();
+	display();
+	struct node *snode=0;
+	snode=(struct node*)malloc(sizeof(struct node));
+	printf("\nEnter newly created node data:");
+	scanf("%d",&snode->data);
+	snode->link=NULL;
+	temp=head;
+	while(temp->link!=0)
+	{
+		temp=temp->link;
+	}
+	temp->link=snode;
+	display();
+	return 0;
+}
