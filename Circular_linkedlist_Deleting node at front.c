ff8200042fe127914ef027575800f3a0ecff50b8
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-06 13:52:04 +0530

    Add files via upload

---
+++ Circular_linkedlist_Deleting node at front.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+};
+struct node *head=0,*tail=0,*temp=0;
+void create()
+{
+	struct node *newnode=0;
+	int i,n;
+	printf("Enter n size:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		newnode=(struct node *)malloc(sizeof(struct node));
+		printf("\nEnter node %d data:",i+1);
+		scanf("%d",&newnode->data);
+		newnode->next=0;
+		if(head==NULL)
+		{
+			head=tail=newnode;
+			tail->next=head;
+		}
+		else
+		{
+			tail->next=newnode;
+			tail=newnode;
+			tail->next=head;
+		}
+	}
+}
+void display()
+{
+	temp=head;
+	do
+	{
+		printf("%d\t",temp->data);
+		temp=temp->next;
+	}
+	while(temp!=head);
+}
+int main()
+{
+	create();
+	display();
+	if(head==0)
+	{
+		printf("\nList is empty");
+		
+	}
+	else if(head==tail)
+	{
+		free(head);
+		head=tail=0;
+	}
+	else
+	{
+		temp=head;
+		head=head->next;
+		free(temp);
+		tail->next=head;
+	}
+	printf("\nAfter deletion:");
+	display();
+	printf("\n%d",tail->next->data);
+	return 0;
+}
